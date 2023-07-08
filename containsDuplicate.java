53acf6bc60947cd8fbe7921a7494d7f0de37ec6d
Crimson-Muffin <sudeep03221@gmail.com>
2023-07-08 14:15:20 +0530

    Added the containsDuplicate file

---
+++ containsDuplicate.java
@@ -0,0 +1,30 @@
+// This is the Solution to the Question 217 in Leetcode
+import java.util.HashSet;
+
+class containsDuplicate{
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> numbers = new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(numbers.contains(nums[i])) return true;
+            else numbers.add(nums[i]);
+        }
+        return false;
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+// Another Solution: Sort the array and check if the adjacent elements are equal
+// class containsDuplicate{
+//     public boolean containsDuplicate(int[] nums){
+//         Arrays.sort(nums);
+//         for(int i=0;i<nums.length-1;i++){
+//             if(nums[i]==nums[i+1]) return true;
+//         }
+//         return false;
+//     }
+// }
+
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
