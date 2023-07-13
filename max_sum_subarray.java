d765e2801ed3d7e1125f87d82091860a7bc0defd
Crimson-Muffin <sudeep03221@gmail.com>
2023-07-13 12:33:33 +0530

    updated by adding comments

--- max_sum_subarray.java
+++ max_sum_subarray.java
@@ -1,7 +1,9 @@
 public class max_sum_subarray {
     public int maxSubArray(int[] nums){
+        // Initializing max sum and curr sum
         int max_sum=nums[0];
         int curr_sum=nums[0];
+        // for loop to find the max sum and curr sum
         for(int i = 1;i<nums.length;i++){
             curr_sum=Math.max(nums[i], nums[i]+curr_sum);
             max_sum=Math.max(curr_sum, max_sum);
