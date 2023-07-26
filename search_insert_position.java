65fbbac81fb391aae73999799a856eb6cdba17e0
Crimson-Muffin <sudeep03221@gmail.com>
2023-07-26 11:00:34 +0530

    Added the solution to the search insert position

---
+++ search_insert_position.java
@@ -0,0 +1,25 @@
+/*Given a sorted array of distinct integers and a target value, 
+return the index if the target is found. If not,
+return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity. */
+
+public class search_insert_position {
+    public int searchInsertPosition(int[] nums, int target){
+        // Using binary search to find the element
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(nums[mid]<target){
+                start = mid + 1;
+            }
+            else if(nums[mid]>target){
+                end = mid - 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return start;
+    }
+}
