7f00798df05169d3c9c88b9b0e4ec52666a379b3
Crimson-Muffin <sudeep03221@gmail.com>
2023-08-04 20:45:37 +0530

    added plus one soln

---
+++ plus_one66.java
@@ -0,0 +1,9 @@
+public class plus_one66 {
+    public static void main(String[] args) {
+        int[] digits = {1,2,3};
+        int n = digits.length;
+        for(int i = n; i>0;i--){
+            
+        }
+    }
+}
