c4fa12f7d3213996b8c41be68ea01bceec1080ba
Crimson-Muffin <sudeep03221@gmail.com>
2023-08-06 19:25:11 +0530

    added comments to Length of last word

--- Length_of_Last_Word58.java
+++ Length_of_Last_Word58.java
@@ -1,6 +1,14 @@
 class Length_of_Last_Word58{
     public int lengthOfLastWord(String s) {
+        // trim() removes leading and trailing spaces
+        // lastIndexOf(" ") returns the index of the last space
         s = s.trim();
+        // return the length of the string minus the index of the last space minus 1
+        // this is because we want the length of the last word
+        // and we can't have the space in the count
+        // we subtract 1 because the index starts at 0
+        // and the length starts at 1
+        // so we need to subtract 1 to get the correct length
         return s.length()-s.lastIndexOf(" ")-1;
     }
 }
