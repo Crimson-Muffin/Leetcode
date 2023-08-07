99b07066eb5c2a903dad8cca6f243e1fe382d01a
Crimson-Muffin <sudeep03221@gmail.com>
2023-08-07 20:02:38 +0530

    added soln

--- plus_one66.java
+++ plus_one66.java
@@ -1,9 +1,44 @@
+class test{
+int[] plusOne(int[] digits){
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
 public class plus_one66 {
+    // int[] plusOne(int[] digits){
+    //     int n = digits.length;
+    //     digits[n-1]=digits[n-1] + 1;
+    //     for(int i = n; i >= 0; --i){
+    //         if(digits[i]<9){
+    //             digits[i]++;
+    //             return digits;
+    //         }
+    //         digits[i]=0;
+    //     }
+    //     int[] newNumber = new int[n+1];
+    //     newNumber[0] = 1;
+    //     return newNumber;
+    // }
     public static void main(String[] args) {
-        int[] digits = {1,2,3};
-        int n = digits.length;
-        for(int i = n; i>0;i--){
-            
+        int[] digit = {9};
+        int n = digit.length;
+        for(int i = 0; i < n; i++){
+            System.out.println(digit[i]);
+        }
+        test t = new test();
+        t.plusOne(digit);
+        // 
+        System.out.println();
+        for(int i = 0; i < digit.length; i++){
+            System.out.println(digit[i]);
         }
     }
 }
