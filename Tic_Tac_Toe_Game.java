01d9c281fd04a9c4e93e21a848a2e188bff4fd08
Crimson-Muffin <sudeep03221@gmail.com>
2023-08-11 22:31:50 +0530

    Updated Tic_tac_toe.java

--- Tic_Tac_Toe_Game.java
+++ Tic_Tac_Toe_Game.java
@@ -1,25 +1,46 @@
 public class Tic_Tac_Toe_Game {
-    // public static void tictactoe(int[][] moves) {
-    //     int max_moves = 9;
-    //     // for(int i = 0; i < moves.length; i++){
-    //     //     if(i%2 == 0){
-    //     //         System.out.println(moves[0]);
-    //     //     }
-    //     // }
-    //     System.out.println(moves[0]);
-    // }
+    class Solution {
+        public String tictactoe(int[][] moves) {
+            int i,arr[][]=new int[3][3];
+            //storing moves
+            for(i=0;i<moves.length;i++)
+                if(i%2==0)
+                    arr[moves[i][0]][moves[i][1]]=1;
+                else
+                    arr[moves[i][0]][moves[i][1]]=-1;
 
-    public static void main(String[] args) {
-        int[][] A ={{0,0},{2,0},{1,1},{2,1},{2,2}};
-        char[][] grid = new char[3][3];
-
-        for(int i=0; i<A.length; i++){
-            if(i%2 == 0){
-                grid[A[i][0]][A[i][1]] = 'X';
+            //checking winner from rows and columns
+            for(i=0;i<3;i++) {
+                if(arr[i][0]==arr[i][1]&&arr[i][1]==arr[i][2])
+                    if(arr[i][0]==1)
+                        return "A";
+                    else if(arr[i][0]==-1)
+                        return "B";
+                if(arr[0][i]==arr[1][i]&&arr[1][i]==arr[2][i])
+                    if(arr[0][i]==1)
+                        return "A";
+                    else if(arr[0][i]==-1)
+                        return "B";
             }
-            else{
-                grid[A[i][0]][A[i][1]] = 'O';
-            }
+            
+            //checking winner from main diagonal
+            if(arr[0][0]==arr[1][1]&&arr[1][1]==arr[2][2])
+                if(arr[0][0]==1)
+                    return "A";
+                else if(arr[0][0]==-1)
+                    return "B";
+            
+            //checking winner from right diagonal
+            if(arr[2][0]==arr[1][1]&&arr[1][1]==arr[0][2])
+                if(arr[2][0]==1)
+                    return "A";
+                else if(arr[2][0]==-1)
+                    return "B";
+            
+            if(moves.length<9)
+                return "Pending";
+            
+            return "Draw";
         }
     }
 }
