154b3fbf9d1592574ce3e7f23714bf7eaec92a69
Crimson-Muffin <sudeep03221@gmail.com>
2023-08-16 19:41:46 +0530

    added addBinary.java

---
+++ addBinary67.java
@@ -0,0 +1,18 @@
+public class addBinary67 {
+    public String addBinary(String a, String b) {
+        // 100 + 111 = (1)011 carry extra both msbs 1 = carry
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j>=0){
+            int sum = carry;
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
