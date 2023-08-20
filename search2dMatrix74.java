ecbcfda7b2aec0b35d19eeaa250be14a5e3fba3c
Crimson-Muffin <sudeep03221@gmail.com>
2023-08-20 19:37:33 +0530

    added 74th problem searching through a 2d matrix

---
+++ search2dMatrix74.java
@@ -0,0 +1,27 @@
+public class search2dMatrix74 {
+    public boolean searchMatrix(int[][] matrix,int target){
+        int row = matrix.length;
+        int col = matrix[0].length;
+        
+        for(int i = 0; i<row; i++){
+            int l = matrix[i][0];
+            int r = matrix[i][col-1];
+            if(l <= target && r>= target){
+                for(int j = 0; j<col; j++){
+                    if(matrix[i][j] == target) return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean searchMatrix1(int[][] matrix,int target){
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<col; j++){
+                if(matrix[i][j] == target) return true;
+            }
+        }
+        return false;   
+    }
+}
