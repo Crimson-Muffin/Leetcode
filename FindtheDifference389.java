1f1fe6c6bf423de2b72c4581cb89b82c1bdd1793
Crimson-Muffin <sudeep03221@gmail.com>
2023-09-25 20:25:24 +0530

    added soln to find the Difference

---
+++ FindtheDifference389.java
@@ -0,0 +1,20 @@
+class FindtheDifference389{
+    public char findTheDifference(String s, String t) {
+        long sum1=0,sum2=0;
+        char[] arr1=s.toCharArray();
+        char[] arr2=t.toCharArray();
+        for(char i:arr1){
+            sum1+=(long)i;
+        }
+        for(char i:arr2){
+            sum2+=(long)i;
+        }
+        return (char)(sum2-sum1);
+    }
+    public char findTheDifference(String s, String t) {
+        char c = 0;
+        for(char cs : s.toCharArray()) c ^= cs;
+        for(char ct : t.toCharArray()) c ^= ct;
+        return c;
+    }
+}
