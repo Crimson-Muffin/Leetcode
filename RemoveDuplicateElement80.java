df1a70befc198747ba0e3996c538eb2161cefc3b
Crimson-Muffin <sudeep03221@gmail.com>
2023-09-30 21:47:00 +0530

    added solutions

---
+++ RemoveDuplicateElement80.java
@@ -0,0 +1,20 @@
+class RemoveDuplicateElement80{
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for(int n: nums){
+            if( i < 2 || n > nums[i-2])
+                nums[i++] = n;
+        }
+        return i;
+    }
+    public int removeDuplicates1(int[] nums) {
+        int j=1;
+        for(int i = 1; i<nums.length; i++ ){
+            if( nums[i] != nums[i-1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
