bea864285b163a78b36385d1e02491e9c76c7b81
Crimson-Muffin <sudeep03221@gmail.com>
2023-10-09 23:13:43 +0530

    edited solns

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -1,5 +1,42 @@
+import java.util.Arrays;
+import java.util.Map;
+
 public class ValidAnagram {
+    // Naive solution using Sorting
     public boolean isAnagram(String s, String t) {
+        char[] schars = s.toCharArray();
+        char[] tchars = t.toCharArray();
+
+        Arrays.sort(schars);
+        Arrays.sort(tchars);
+
+        return Arrays.equals(schars, tchars);
+    }
+    
+    // Using HashMap
+    public boolean isAnagram1(String s, String t){
+        Map<Character, Integer> count = new HashMap<>();
+
+
+        for(char x: s.toCharArray()){
+            count.put(x, count.getOrDefault(x, 0)+1);
+        }
+
+        for(char x: t.toCharArray()){
+            count.put(x, count.getOrDefault(x,0)-1);
+        }
+
+        for(int val: count.values()){
+            if(val!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+    public boolean isAnagram2(String s, String t) {
         int[] count = new int[26];
         
         // Count the frequency of characters in string s
