eaeaaceb708acd65453db8b33ce8f6a1282c37b1
Crimson-Muffin <sudeep03221@gmail.com>
2023-11-26 22:06:14 +0530

    added Solutions to problems

---
+++ SubtreeOfAnotherTree572.java
@@ -0,0 +1,13 @@
+public class SubtreeOfAnotherTree572 {
+    public boolean isSubTree(TreeNode s, TreeNode t){
+        if(s == null) return false;
+        if(isSameTree(s,t)) return true;
+        return isSubTree(s.left, t) || isSubTree(s.right, t);
+    }
+    public boolean isSameTree(TreeNode s, TreeNode t){
+        if(s == null && t == null) return true;
+        if(s == null || t == null) return false;
+        if(s.val != t.val) return false;
+        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    }
+}
